e9c3fc4f9bb0eb2109b60e15469d4d8e134ea06b
vasik042 <56596110+vasik042@users.noreply.github.com>
2020-03-12 00:37:02 +0200

    JDBC and Servlets homework are done

---
+++ src/main/java/IndexServlet.java
@@ -0,0 +1,19 @@
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import java.io.IOException;
+
+@WebServlet("/registration")
+public class IndexServlet extends HttpServlet{
+
+
+        @Override
+        protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+    }
+
+        protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+        request.getRequestDispatcher("register.jsp").forward(request, response);
+    }
+}
